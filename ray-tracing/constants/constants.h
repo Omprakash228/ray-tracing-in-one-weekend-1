1bedf62c86de08f4a5c8b4d77ac0e2338995931e
Omprakash <omprakashnarayanan302@gmail.com>
2024-01-13 21:05:57 -0500

    initial commit

---
+++ ray-tracing/constants/constants.h
@@ -0,0 +1,38 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+// Usings
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline double degrees_to_radians(double degrees) {
+	return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+	// Returns a random real number in [0, 1)
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+	// Returns a random real number in [min, max)
+	return min + (max - min) * random_double();
+}
+
+// Common Headers
+#include "../interval/interval.h"
+#include "../ray/ray.h"
+#include "../vec3/vec3.h"
+
+#endif
